5115d00855f86eda78129d917ef640a6c82f57f9
František Nečas <frantisek.necas@protonmail.com>
2020-10-28 10:15:17 +0100

    Implement backtracking algorithm for solving the problem
    Signed-off-by: František Nečas <frantisek.necas@protonmail.com>

--- hexagon.py
+++ hexagon.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Set
 
 
 class Hex:
@@ -56,6 +56,11 @@ class HexMap:
             for r in range(r1, r2 + 1):
                 self._map[Hex(q, r, -q - r)] = 0
 
+        # Hash maps used for quickly checking whether the map satisfies the condition
+        self.rows: List[Set[int]] = [set() for _ in range(self.diameter)]
+        self.diags1: List[Set[int]] = [set() for _ in range(self.diameter)]
+        self.diags2: List[Set[int]] = [set() for _ in range(self.diameter)]
+
     def get_direction(self, start: Hex, direction: int):
         """Creates an iterator over a row/diagonal in the given direction."""
         # Rollback to the start of the row
@@ -91,3 +96,106 @@ class HexMap:
                 starting_hexagon = starting_hexagon.neighbour(1)
         print()
 
+    def check_condition(self, choices: List[int]):
+        """Checks whether the current state satisfies the task condition."""
+        for sets in (self.rows, self.diags1, self.diags2):
+            for value_set in sets:
+                if len(value_set) != len(choices):
+                    return False
+        return True
+
+    def set_value(self, current_node: Hex, new_value: int, row_index: int, diag1_index: int, diag2_index: int):
+        """Sets the value of the current_node to new_value.
+
+        Returns whether the value was set successfully (returns False if
+        the number has already been used in the current row/diagonal).
+        """
+        if new_value != 0:
+            if new_value in self.rows[row_index] or new_value in self.diags1[diag1_index] or new_value in \
+                    self.diags2[diag2_index]:
+                return False
+            self.rows[row_index].add(new_value)
+            self.diags1[diag1_index].add(new_value)
+            self.diags2[diag2_index].add(new_value)
+        self._map[current_node] = new_value
+        return True
+
+    def reset_value(self, current_node: Hex, row_index: int, diag1_index: int, diag2_index: int):
+        """Resets a node to its initial state."""
+        current_value = self._map[current_node]
+        self._map[current_node] = 0
+        if current_value != 0:
+            self.rows[row_index].remove(current_value)
+            self.diags1[diag1_index].remove(current_value)
+            self.diags2[diag2_index].remove(current_value)
+
+    def is_invalid_row_or_diag(self, direction: int, choices: List[int], row_index: int,
+                               diag1_index: int, diag2_index: int):
+        """Checks if the recently finished row or diagonal is valid."""
+        invalid = False
+        if direction % 3 == 0:
+            # We just finished a row, check the rows
+            if len(choices) != len(self.rows[row_index]):
+                invalid = True
+        elif direction % 3 == 1:
+            # We just finished top right - bottom left diag
+            if len(choices) != len(self.diags2[diag2_index]):
+                invalid = True
+        elif direction % 3 == 2:
+            # We just finished top left - bottom right diag
+            if len(choices) != len(self.diags1[diag1_index]):
+                invalid = True
+        return invalid
+
+    def _walk(self, current_node: Hex, direction: int, remaining_steps: int, level: int,
+              choices: List[int], choices_index: int):
+        row_index = current_node.r + self.radius
+        diag1_index = current_node.s + self.radius
+        diag2_index = current_node.q + self.radius
+        # Base case for recursion
+        if current_node == Hex(0, 0, 0):
+            for new_value in (0, choices[choices_index]):
+                if not self.set_value(current_node, new_value, row_index, diag1_index, diag2_index):
+                    continue
+                if self.check_condition(choices):
+                    self.print()
+                self.reset_value(current_node, row_index, diag1_index, diag2_index)
+            return
+
+        # We can either leave the current cell empty (0) or fill it with the next number
+        for new_value in (0, choices[choices_index]):
+            new_choices_index = choices_index
+            new_level = level
+            new_direction = direction
+            new_remaining_steps = remaining_steps
+            next_node = current_node
+
+            if new_value != 0:
+                new_choices_index = (choices_index + 1) % len(choices)
+            if not self.set_value(current_node, new_value, row_index, diag1_index, diag2_index):
+                continue
+            if remaining_steps == 0:
+                # One direction finished, lets try to cut down the search tree
+                if self.is_invalid_row_or_diag(direction, choices, row_index, diag1_index, diag2_index):
+                    self.reset_value(current_node, row_index, diag1_index, diag2_index)
+                    continue
+
+                # Change direction
+                new_direction = (direction + 1) % 6
+                new_remaining_steps = self.radius - level - (1 if new_direction == 5 else 0)
+                # Last element before center, there is no steps to go to in top right direction, just skip it.
+                if new_direction == 5 and new_remaining_steps == 0:
+                    new_direction = 0
+                    new_remaining_steps = 1
+                if new_direction == 0:
+                    new_level += 1
+            if new_remaining_steps != 0:
+                next_node = current_node.neighbour(new_direction)
+                new_remaining_steps -= 1
+            self._walk(next_node, new_direction, new_remaining_steps, new_level, choices, new_choices_index)
+            self.reset_value(current_node, row_index, diag1_index, diag2_index)
+
+    def solve_problem(self, solve_for: int):
+        """Solves the hexagon problem trying to fit `solve_for` numbers into the hexagon."""
+        current_node = Hex(0, -self.radius, self.radius)
+        self._walk(current_node, 0, self.radius, 0, [x + 1 for x in range(solve_for)], 0)
